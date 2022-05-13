16166c4e7bfbf55e2983b9a0c35aaf045fd66bd5
karam-zomlut <zomlutk@gmail.com>
2022-05-13 22:49:17 +0300

    Setup the Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Pages from "./pages/Pages";
+import Pages from './pages/Pages';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
       <Pages />
     </div>
   );
---
+++ src/components/Categories.jsx
@@ -0,0 +1,11 @@
+import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
+import { GiNoodles, GiChopsticks } from 'react-icons/gi';
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div>Categories</div>
+  )
+}
+
+export default Categories
